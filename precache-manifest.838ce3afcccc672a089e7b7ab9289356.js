5665e2f4ce100674e365be8bbb5ba1131af78b3d
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2021-02-05 17:19:30 -0600

    Updates

---
+++ precache-manifest.838ce3afcccc672a089e7b7ab9289356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b56151e5288fa125209ce0f264de3b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b09afaef95740f0b5ab",
+    "url": "/static/css/main.d0999c69.chunk.css"
+  },
+  {
+    "revision": "209c64ac0364badd1884",
+    "url": "/static/js/2.badbc686.chunk.js"
+  },
+  {
+    "revision": "bd16ffa8a645b42adcb7fc65d4c790f8",
+    "url": "/static/js/2.badbc686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b09afaef95740f0b5ab",
+    "url": "/static/js/main.3323b760.chunk.js"
+  },
+  {
+    "revision": "142b697ed77b435eaccc",
+    "url": "/static/js/runtime-main.efa8e349.js"
+  }
+]);
